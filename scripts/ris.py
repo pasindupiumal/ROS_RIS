88fa95359dfe7c50f6f0abc743949f0814046800
pasindu <pasindu0996@gmail.com>
2020-04-22 18:34:23 +0530

    Increased laser scan range

--- scripts/ris.py
+++ scripts/ris.py
@@ -20,6 +20,7 @@ forward_speed_ = 0.18
 current_distance_ = 0
 
 state_ = 0
+stage_ = 1
 
 t0_ = None
 t1_ = None
@@ -30,14 +31,20 @@ target_angle_ = 0
 kP_ = 1.0
 angular_velocity_ = 0
 target_rad_ = target_angle_ * math.pi / 180
+is_stopped = False
 should_rotate_ = 0
 
 regions_ = {
-        'right': 0,
-        'fright': 0,
-        'front': 0,
-        'fleft': 0,
-        'left': 0,
+        'one': 0,
+        'two': 0,
+        'three': 0,
+        'four': 0,
+        'five': 0,
+        'six': 0,
+        'seven': 0,
+        'eight': 0,
+        'nine': 0,
+        'ten': 0,
 }
 state_dict_ = {
     0: 'Stopped moving',
@@ -45,6 +52,11 @@ state_dict_ = {
     2: "Moving right",
     3: "Moving left",
 }
+stage_dict_ = {
+    1: 'Stage 1',
+    2: 'Stage 2',
+}
+
 
 
 #Subscriber callback methods
@@ -57,19 +69,32 @@ def change_state(state):
         state_ = state
 
 
+def change_stage(stage):
+
+    global stage_, stage_dict_
+    if stage is not stage_:
+        print ("Initiated - {}".format(stage_dict_[stage]))
+        state_ = state
+
+
 def laserCallback(msg):
 
     global regions_, angular_velocity_, yaw_, should_rotate_
 
     regions_ = {
-        'right':  min(min(msg.ranges[0:143]), 10),
-        'fright': min(min(msg.ranges[144:287]), 10),
-        'front':  min(min(msg.ranges[288:431]), 10),
-        'fleft':  min(min(msg.ranges[432:575]), 10),
-        'left':   min(min(msg.ranges[576:719]), 10),
+        'one':  min(min(msg.ranges[0:71]), 10),
+        'two': min(min(msg.ranges[72:143]), 10),
+        'three':  min(min(msg.ranges[144:215]), 10),
+        'four':  min(min(msg.ranges[216:287]), 10),
+        'five':   min(min(msg.ranges[288:359]), 10),
+        'six':  min(min(msg.ranges[360:431]), 10),
+        'seven': min(min(msg.ranges[432:503]), 10),
+        'eight':  min(min(msg.ranges[504:575]), 10),
+        'nine':  min(min(msg.ranges[576:647]), 10),
+        'ten':   min(min(msg.ranges[648:719]), 10),
     }
 
-    print("DW - {} | AV - {} | Yaw - {} | SR - {}".format(regions_['right'], angular_velocity_, yaw_, should_rotate_))
+    print("DW - {} | AV - {} | Yaw - {} | SR - {}".format(regions_['three'], angular_velocity_, yaw_, should_rotate_))
 
 
 def odomCallback(msg):
@@ -88,7 +113,7 @@ def odomCallback(msg):
 
 def stop_moving():
 
-    global velocity_publisher_
+    global velocity_publisher_, is_stopped, yaw_, kP_, target_rad_, angular_velocity_
 
     msg = Twist()
 
@@ -97,9 +122,20 @@ def stop_moving():
     msg.linear.z = 0
     msg.angular.x = 0
     msg.angular.y = 0
-    msg.angular.z = 0  
+
+    if ( abs ( target_rad_ - yaw_) > 0.0001 ):
+
+        msg.angular.z = kP_  * (target_rad_ - yaw_)
+    
+    else:
+
+        msg.angular.z = 0.0
 
     velocity_publisher_.publish(msg)
+
+    if  abs(angular_velocity_) < 0.0000001:
+
+        change_state(2)
 
 
 def move_right():
@@ -154,13 +190,44 @@ def move_forward():
 
 
 
+def go_to_door():
+
+    global regions_, is_stopped
+
+    if ( is_stopped == False ):
+
+            if regions_['three'] == 10:
+
+                is_stopped = True
+
+            if regions_['one'] > 1.1:
+
+                change_state(2)
+                move_right()
+
+            elif regions_['one'] < 1.0:
+
+                change_state(3)
+                move_left()
+            
+            else:
+
+                change_state(1)
+                move_forward()
+
+    else:
+
+        change_state(0)
+        stop_moving()
+
+
+
 def main():
 
     #Declare variables
 
-    global velocity_publisher_, laser_subscriber_, regions_, odom_subscriber_
-    front_wall_distance = 0.5
-    laser_max = 10.0
+    global velocity_publisher_, laser_subscriber_, regions_, odom_subscriber_, stage_
+
 
     #Initialize ros node
 
@@ -168,7 +235,7 @@ def main():
 
     velocity_publisher_ = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
 
-    laser_subscriber_ = rospy.Subscriber('/scan', LaserScan, laserCallback )
+    laser_subscriber_ = rospy.Subscriber('/scan', LaserScan, laserCallback)
 
     odom_subscriber_ = rospy.Subscriber('/odom', Odometry, odomCallback)
 
@@ -178,22 +245,15 @@ def main():
 
     while not rospy.is_shutdown():
 
-        if regions_['right'] > 1.1:
+        if ( stage_ == 1 ):
 
-            change_state(2)
-            move_right()
+            go_to_door()
 
-        elif regions_['right'] < 1.0:
+        elif ( stage_ == 2 ):
 
-            change_state(3)
-            move_left()
-        
-        else:
+            print "Started stage two"
 
-            change_state(1)
-            move_forward()
 
-        
         rate.sleep()
 
 
